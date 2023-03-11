98eb830c968a9118d66892def3ec476b26ea5e96
AdrianKamulegeya <adriankamulegeya@gmail.com>
2023-03-11 20:18:17 +0000

    add config.js file for nav and header

---
+++ src/config.js
@@ -0,0 +1,44 @@
+module.exports = {
+    email: 'adriankamulegeya@gmail.com',
+    socialMedia: [
+        {
+            name: 'GitHub',
+            url: 'https://github.com/AdrianKamulegeya'
+        },
+        {
+            name: 'Linkedin',
+            url: 'https://www.linkedin.com/in/adrian-kamulegeya-461779128',
+        },
+        {
+            name: 'Twitter',
+            url: 'https://twitter.com/impracticalgmr'
+        },
+        {
+            name: 'Instagram',
+            url: 'https://www.instagram.com/impracticalgmr',
+        },
+    ],
+    navLinks: [
+        {
+            name: 'About',
+            url: '/#about',
+        },
+        {
+            name: 'Experience',
+            url: '/#jobs',
+        },
+        {
+            name: 'Projects',
+            url: '/#projects',
+        },
+        {
+            name: 'Contact',
+            url: '/#contact',
+        },
+    ],
+    colors: {
+        green: '#64ffda',
+        navy: '#0a192f',
+        darkNavy: '#020c1b',
+    },
+}
